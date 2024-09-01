96df8b18c7c3603a0e5c0d97c51e68a615ef46dc
sukhrobdeveloper <sukhrobtokhirov006@gmail.com>
2024-09-01 13:29:09 +0500

    Project SetUp

---
+++ security-service/src/test/java/sukhrob/developer/security_service/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sukhrob.developer.security_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
